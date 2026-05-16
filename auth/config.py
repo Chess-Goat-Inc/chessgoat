e390048d0b92c8ca587b5375643c93c630e067a0
fenton <viktorken8264@gmail.com>
2026-05-16 12:51:46 +0300

    add authorization and authototication service

---
+++ auth/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY_ACCESS = os.getenv("SECRET_KEY_ACCESS")
+SECRET_KEY_REFRESH = os.getenv("SECRET_KEY_REFRESH")
+ALGORITH = os.getenv("ALGORITHM")
