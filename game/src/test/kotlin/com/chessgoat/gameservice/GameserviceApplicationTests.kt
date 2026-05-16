f86dd840b9e058fcb531264fc581b9221c7361c8
PavMash <ulpam2005@gmail.com>
2026-05-16 10:08:45 +0300

    Game - Add unit tests for chess business logic

---
+++ game/src/test/kotlin/com/chessgoat/gameservice/GameserviceApplicationTests.kt
@@ -0,0 +1,52 @@
+package com.chessgoat.gameservice
+
+import com.chessgoat.gameservice.domain.PlayerColor
+import com.chessgoat.gameservice.engine.ChessLibAdapter
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+@SpringBootTest
+class GameserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+    @Test
+    fun `initial position created correctly`() {
+        val engine = ChessLibAdapter()
+        val state = engine.createInitialState()
+
+        assertEquals(
+            PlayerColor.WHITE,
+            state.turn
+        )
+    }
+
+    @Test
+    fun `e2e4 should be legal`() {
+        val engine = ChessLibAdapter()
+        val state = engine.createInitialState()
+        val result = engine.applyMove(
+            state,
+            "e2e4"
+        )
+
+        assertTrue(result.success)
+    }
+
+    @Test
+    fun `illegal move rejected`() {
+        val engine = ChessLibAdapter()
+        val state = engine.createInitialState()
+        val result = engine.applyMove(
+            state,
+            "e2e5"
+        )
+
+        assertFalse(result.success)
+    }
+}
