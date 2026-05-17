6d2a69b1590c774935be3a319df7754bae41e8c6
Alexander Blinov <mhdtadev@gmail.com>
2026-05-17 12:45:16 +0300

    fix: Added CORS

--- auth/internal/config/config.go
+++ auth/internal/config/config.go
@@ -3,26 +3,31 @@ package config
 import (
 	"crypto/rsa"
 	"os"
+	"strings"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-
 type Config struct {
+	Routes struct {
+		CORSAddresses []string
+	}
+
 	Database struct {
-		Url     string
+		Url      string
 		User     string
 		Password string
 		Name     string
 	}
 
 	Secret *rsa.PrivateKey
-
 }
 
 var AppConfig Config
 
 func Load() {
+	AppConfig.Routes.CORSAddresses = parseCommaSeparatedEnv("CORS_ADDRS")
+
 	AppConfig.Database.Url = os.Getenv("DB_URL")
 	AppConfig.Database.User = os.Getenv("DB_USER")
 	AppConfig.Database.Password = os.Getenv("DB_PASSWORD")
@@ -34,11 +39,25 @@ func Load() {
 	}
 
 	private, err := jwt.ParseRSAPrivateKeyFromPEM(privateKeyBytes)
-	
+
 	if err != nil {
 		panic(err)
 	}
-	
-	AppConfig.Secret = private;
 
+	AppConfig.Secret = private
+
+}
+
+func parseCommaSeparatedEnv(name string) []string {
+	values := strings.Split(os.Getenv(name), ",")
+	result := make([]string, 0, len(values))
+
+	for _, value := range values {
+		value = strings.TrimSpace(value)
+		if value != "" {
+			result = append(result, value)
+		}
+	}
+
+	return result
 }
